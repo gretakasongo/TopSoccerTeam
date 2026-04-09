a789d33aa6bedbf36c6d29b52e2affdd360f59cf
gretakasongo <gretakasongo07@gmail.com>
2026-04-09 15:08:51 +0100

    displaying array content in text view

--- app/src/main/java/com/example/topsoccerteam/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteam/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.topsoccerteam
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Arrays
+import kotlin.toString
 
 class MainActivity : AppCompatActivity() {
     val teams = arrayOf<String>("Man Sundowns Fc","Orlando Pirates","Liverpool","Man city","Chelsea")
@@ -12,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val name = findViewById<TextView>(R.id.name)
+        //reassigning top soccer team in array
+        teams [0] = "Man Sundowns Fc"
+        // displaying top soccer team in text view on UI
+        name.text = Arrays.toString(teams)
+
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
