a2b1b1c29f032f3b11b88772906b65d4dbb6413c
gretakasongo <gretakasongo07@gmail.com>
2026-04-10 13:24:06 +0100

    added function to array values and added grammators to logging function

--- app/src/main/java/com/example/topsoccerteam/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteam/MainActivity.kt
@@ -2,44 +2,50 @@ package com.example.topsoccerteam
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.v
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
+// create function to log array values
+    fun logArrValues(arr: Array<String>,limit: Int=0){
+        // if statement to check if limit is 0
+        if (limit == 0)
+            Log.v("Array Values", Arrays.toString(arr))
+        else
+
+        Log.v("Array Values", Arrays.toString(arr.sliceArray(0..limit-1)))
+    }
 
 class MainActivity : AppCompatActivity() {
     val teams = arrayOf<String>("Manchester united","Real Madrid","PSG","Bayern Munich","Liverpool")
-    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val teamsTxt = findViewById<TextView>(R.id.teamsDisplay)
+        val teamsTxt = findViewById<TextView>(R.id.teamsTxt)
+        var teamsDisplay = ""
         var count = 0
-
-
+        //calling logArrValues function
+        logArrValues(teams,2)
         //reassigning top soccer team in array
         teams [0] = "Man Sundowns Fc"
+
         // while loop to display top soccer team in array
-        while (count < teams.count()){
-            var teamsDisplay = "${teams[count]}\n"
-            count++
-        }
-        // variable to display top soccer team
-        var teamsDisplay = ""
-        teamsDisplay += "${teams[0]}\n"
-        teamsDisplay += "${teams[1]}\n"
-        teamsDisplay += "${teams[2]}\n"
-        teamsDisplay += "${teams[3]}\n"
-        teamsDisplay += "${teams[4]}\n"
+//        while (count < teams.count()){
+//            var teamsDisplay = "${teams[count]}\n"
+//            count++
+       // }
+        //for loop to display top soccer team in array
+    for (team in teams){
+        teamsDisplay += "${teams}\n"
+    }
         // displaying top soccer team in text view on UI
         teamsTxt.text = teamsDisplay
-        teamsTxt.text = Arrays.toString(teams)
-        while (teamsDisplay.length > 0)
-
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
