335c10a826203f25edf2c8780c3f34450bdf5d71
gretakasongo <gretakasongo07@gmail.com>
2026-04-15 14:40:08 +0100

    function that calls the longest string

--- app/src/main/java/com/example/topsoccerteam/MainActivity.kt
+++ app/src/main/java/com/example/topsoccerteam/MainActivity.kt
@@ -10,18 +10,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Arrays
+import kotlin.math.log
+
 // create function to log array values
 @SuppressLint("SuspiciousIndentation")
-fun logArrValues(arr: Array<String>, limit: Int=0){
-        // if statement to check if limit is 0
-        if (limit == 0)
-            Log.v("Array Values", Arrays.toString(arr))
-        else
-
-        Log.v("Array Values", Arrays.toString(arr.sliceArray(0..limit-1)))
-    }
-    // Create function or longest array display here
-
+//fun logArrValues(arr: Array<String>, limit: Int=0){
+//        // if statement to check if limit is 0
+//        if (limit == 0)
+//            Log.v("Array Values", Arrays.toString(arr))
+//        else
+//
+//        Log.v("Array Values", Arrays.toString(arr.sliceArray(0..limit-1)))
+//    }
+// looks for the longest entry in string array
+fun getLongestString(arr: Array<String>): String {
+    var longestLength = 0
+    var longestElement = ""
+    for (element in arr) {
+        if (element.count() > longestLength) {
+            longestLength = element.count()
+            longestElement = element
+        }
+        }
+    return longestElement
+}
 
 class MainActivity : AppCompatActivity() {
     val teams = arrayOf<String>("Manchester united","Real Madrid","PSG","Bayern Munich","Liverpool")
@@ -33,10 +45,12 @@ class MainActivity : AppCompatActivity() {
         val teamsTxt = findViewById<TextView>(R.id.teamsTxt)
         var teamsDisplay = ""
         var count = 0
-        //calling logArrValues function
-        logArrValues(teams)
-        logArrValues(teams,3)
-        // Call longest array function here
+//        //calling log array Values function
+//        logArrValues(teams)
+//        logArrValues(teams,3)
+        //call the method to find the longest name in array
+        var longestName = getLongestString(teams)
+        Log.v("Longest Name",longestName)
 
         //reassigning top soccer team in array
         teams [0] = "Man Sundowns Fc"
